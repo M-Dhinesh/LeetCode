1a55a772e9dd1404dc1f67c990598c12b5f8e919
Dhinesh M <73809596+M-Dhinesh@users.noreply.github.com>
2024-03-18 21:57:36 +0530

    Update and rename Palindrome Number-9.py to Remove_Duplicates_from_Sorted_Array_26.cs

---
+++ Remove_Duplicates_from_Sorted_Array_26.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+       int[] unique = nums.Distinct().ToArray();
+       for (int i=0;i<unique.Length;i++){
+           nums[i]=unique[i];
+       }
+        return unique.Length;
+    }
+}
