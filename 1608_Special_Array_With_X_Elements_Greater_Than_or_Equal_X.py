bcda045b653cfce80a3d7520f63945edd2c900b3
Dhinesh M <73809596+M-Dhinesh@users.noreply.github.com>
2024-05-27 14:44:11 +0530

    Create 1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py

---
+++ 1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.py
@@ -0,0 +1,10 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        for n in range(len(nums)+1):
+            cnt = 0
+            for i in nums:
+                if i >= n:
+                    cnt += 1
+            if cnt == n: 
+                return cnt
+        return -1
