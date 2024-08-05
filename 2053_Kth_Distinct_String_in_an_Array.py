eb98da7466fd9ddb02b558f153e74b22acdfe5d5
Dhinesh M <73809596+M-Dhinesh@users.noreply.github.com>
2024-08-06 00:20:14 +0530

    Update 2053_Kth_Distinct_String_in_an_Array.py

--- 2053_Kth_Distinct_String_in_an_Array.py
+++ 2053_Kth_Distinct_String_in_an_Array.py
@@ -1,1 +1,4 @@
-
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        lst = [i for i,j in Counter(arr).items() if j == 1 ]
+        return "" if k > len(lst) else lst[k-1]
